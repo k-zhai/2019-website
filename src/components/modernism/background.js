6c02d0fd3a7575fc0a772e2798923d90c9674a34
geomaster453 <austin.wei@lakesideschool.org>
2019-10-12 18:36:24 -0500

    Progress 10/12

--- src/components/modernism/background.js
+++ src/components/modernism/background.js
@@ -1,8 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import InViewMonitor from 'react-inview-monitor';
+//import MtSvgLines from 'react-mt-svg-lines';
+import '../../../node_modules/animate.css/animate.min.css';
 
 import HeaderShape from '../../images/HeaderShape.svg';
 import TextHeaderShapes from '../../images/TextHeaderShapes.svg';
 
+import Img from 'react-image';
+
+const svgStyle = {
+	width: '100vw',
+	top: '-10vw',
+	zIndex: '0',
+};
+
+const svgStyle2 = {
+	position: 'absolute',
+	width: '100vw',
+	top: '-15vw',
+	zIndex: '0',
+};
+
+const TopBackground = () => {
+	return <HeaderShape style={svgStyle} />;
+};
+
+const BottomBackground = () => {
+	return <TextHeaderShapes style={svgStyle2} />;
+};
+
+/*const TopBackgroundArtAnimated = () => (
+	<InViewMonitor classNameNotInView="vis-hidden" classNameInView="animated rollIn">
+		<TopBackground />
+	</InViewMonitor>
+);*/
+
+const BottomBackgroundArtAnimated = () => (
+	<InViewMonitor classNameNotInView="vis-hidden" classNameInView="animated rotateIn">
+		<BottomBackground />
+	</InViewMonitor>
+);
+
+export { TopBackground, BottomBackgroundArtAnimated };
+
+/*
 const TopBackgroundArt = () => {
 	return <img id="top-background-art" src={HeaderShape} alt="" />;
 };
@@ -11,4 +53,4 @@ const BottomBackgroundArt = () => {
 	return <img id="bottom-background-art" src={TextHeaderShapes} alt="" />;
 };
 
-export { TopBackgroundArt, BottomBackgroundArt };
+export { TopBackgroundArt, BottomBackgroundArt };*/
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 // import { Link } from 'gatsby';
 
 import '../components/layout.css';
+import '../../node_modules/animate.css/animate.min.css';
 import '../components/modernism/modernism.css';
 
 import SEO from '../components/seo';
@@ -11,9 +13,53 @@ import FAQ from '../components/modernism/faq/faq';
 import Schedule from '../components/schedule';
 import SponsorBox from '../components/sponsorBox';
 
+import InViewMonitor from 'react-inview-monitor';
+
+import { TopBackground, BottomBackgroundArtAnimated } from '../components/modernism/background';
+import HeaderInfo from '../components/modernism/headerInfo';
+/*
 import { TopBackgroundArt, BottomBackgroundArt } from '../components/modernism/background';
 import HeaderInfo from '../components/modernism/headerInfo';
+*/
 
+const Container = styled.div`
+	width: 100%;
+`;
+
+const TopPage = () => {
+	return (
+		<Container>
+			<TopBackground />
+			<HeaderInfo />
+		</Container>
+	);
+};
+
+const TopPageAnimated = () => (
+	<InViewMonitor classNameNotInView="vis-hidden" classNameInView="animated rollIn">
+		<TopPage />
+	</InViewMonitor>
+);
+
+const IndexPage = () => {
+	return (
+		<Container>
+			<TopPageAnimated />
+			<BottomBackgroundArtAnimated />
+			<Welcome></Welcome>
+
+			<FAQ></FAQ>
+
+			<Schedule></Schedule>
+
+			<SponsorBox></SponsorBox>
+		</Container>
+	);
+};
+
+export default IndexPage;
+
+/*
 const IndexPage = () => (
 	<div>
 		<SEO title="" />
@@ -36,4 +82,4 @@ const IndexPage = () => (
 	</div>
 );
 
-export default IndexPage;
+export default IndexPage;*/
